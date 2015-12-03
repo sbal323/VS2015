a69863aab2bb86f2c346d2fb59c1885f2af35276
Sergii Balog <bal323@mail.ru>
2015-12-03 23:48:09 +0200

    context added

---
+++ src/WebApplication/Models/EmployeesDb.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace WebApplication.Models
+{
+    public class EmployeesDb
+    {
+        public static MongoCollection<Employee> Open()
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var server = client.GetServer();
+            var db = server.GetDatabase("Employees");
+            return db.GetCollection<Employee>("employees");
+
+        }
+    }
+}
